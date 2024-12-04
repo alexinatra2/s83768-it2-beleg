475123ff90d202b35423441d0911aa2e0c304285
alexinatra2 <a.holzknecht@gmx.de>
2024-12-04 08:25:18 +0100

    implement RtpPacket.setRtpHeader

--- src/rtp/RtpPacket.java
+++ src/rtp/RtpPacket.java
@@ -11,6 +11,27 @@ public class RtpPacket extends RtpPacketDemo {
 
     @Override
     void setRtpHeader() {
+        // Byte 0: Version (2), Padding (0), Extension (0), CC (0)
+        // Version = 2, Padding = 0, Extension = 0, CC = 0
+        header[0] = (byte) (Version << 6);
 
+        // Byte 1: Marker (1 Bit), Payload Type (7 Bits)
+        header[1] = (byte) ((Marker << 7) | (PayloadType & 0x7F)); // Marker und Payload-Type setzen
+
+        // Byte 2-3: Sequence Number (16 Bits)
+        header[2] = (byte) (SequenceNumber >> 8); // High Byte
+        header[3] = (byte) (SequenceNumber);      // Low Byte
+
+        // Byte 4-7: Timestamp (32 Bits)
+        header[4] = (byte) (TimeStamp >> 24);
+        header[5] = (byte) (TimeStamp >> 16);
+        header[6] = (byte) (TimeStamp >> 8);
+        header[7] = (byte) (TimeStamp);
+
+        // Byte 8-11: SSRC (32 Bits)
+        header[8] = (byte) (Ssrc >> 24);
+        header[9] = (byte) (Ssrc >> 16);
+        header[10] = (byte) (Ssrc >> 8);
+        header[11] = (byte) (Ssrc);
     }
 }
