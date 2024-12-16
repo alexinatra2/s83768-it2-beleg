3b6e54af5641077277674654f7a7b67acc8f6434
alexinatra2 <a.holzknecht@gmx.de>
2024-12-16 14:50:20 +0100

    add FecHandler implementation

--- src/rtp/FecHandler.java
+++ src/rtp/FecHandler.java
@@ -1,6 +1,7 @@
 package rtp;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class FecHandler extends FecHandlerDemo {
     public FecHandler(int size) {
@@ -13,11 +14,29 @@ public class FecHandler extends FecHandlerDemo {
 
     @Override
     boolean checkCorrection(int nr, HashMap<Integer, RtpPacket> mediaPackets) {
-        return false;
+        Integer fecPacketNr = fecNr.get(nr);
+        if (fecStack.get(fecPacketNr) == null) {
+            return false;
+        }
+        List<Integer> fecPacketNumbers = fecList.get(nr);
+        for (Integer fecNumber : fecPacketNumbers) {
+            if (fecNumber != nr && mediaPackets.get(fecNumber) == null) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     RtpPacket correctRtp(int nr, HashMap<Integer, RtpPacket> mediaPackets) {
-        return null;
+        Integer fecPacketNr = fecNr.get(nr);
+        FecPacket correctedFecPacket = fecStack.get(fecPacketNr);
+        List<Integer> fecPacketNumbers = fecList.get(nr);
+        for (Integer packetNumber : fecPacketNumbers) {
+            if (packetNumber != nr) {
+                correctedFecPacket.addRtp(mediaPackets.get(packetNumber));
+            }
+        }
+        return correctedFecPacket.getLostRtp(nr);
     }
 }
